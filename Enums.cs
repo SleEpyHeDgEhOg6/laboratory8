a64a172514651c83e9df428bcae0652ca7d4bc73
Vladislava <122866844+SleEpyHeDgEhOg6@users.noreply.github.com>
2025-12-26 23:48:49 +0300

    Create Enums.cs

---
+++ Enums.cs
@@ -0,0 +1,18 @@
+namespace AnimalLibrary
+{
+    [Comment("Классификация животных по типу питания")]
+    public enum eClassificationAnimal
+    {
+        Herbivores,  // Травоядные
+        Carnivores,  // Хищники
+        Omnivores    // Всеядные
+    }
+
+    [Comment("Типы любимой пищи животных")]
+    public enum eFavoriteFood
+    {
+        Meat,       
+        Plants,     
+        Everything  
+    }
+}
